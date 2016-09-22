74999b5d3d10636eefff585fc926f0604cab5082
WindomZ <zwdsdtc@gmail.com>
2016-09-22 17:40:32 +0800

    style: rename code to country

--- regexutil/util.go
+++ regexutil/util.go
@@ -2,33 +2,33 @@ package regexutil
 
 import "strings"
 
-// StandardTelephoneNumber get International telephone number from country`s code and tel
-func StandardTelephoneNumber(code, tel string) string {
+// StandardTelephoneNumber get International telephone number from country and tel
+func StandardTelephoneNumber(country, tel string) string {
 	if len(tel) == 0 {
 		return tel
 	}
-	code = strings.Replace(code, "+", "", -1)
+	country = strings.Replace(country, "+", "", -1)
 	tel = strings.Replace(strings.TrimSpace(tel), " ", "", -1)
-	if strings.HasPrefix(tel, "+"+code) {
-	} else if len(code) != 0 {
-		if strings.HasPrefix(tel, code) {
+	if strings.HasPrefix(tel, "+"+country) {
+	} else if len(country) != 0 {
+		if strings.HasPrefix(tel, country) {
 			tel = "+" + tel
 		} else {
-			tel = "+" + code + tel
+			tel = "+" + country + tel
 		}
 	}
 	return strings.Replace(tel, "-", "", -1)
 }
 
-// BaseTelephoneNumber get base telephone number from country`s code and tel
-func BaseTelephoneNumber(code, tel string) string {
+// BaseTelephoneNumber get base telephone number from country`s country and tel
+func BaseTelephoneNumber(country, tel string) string {
 	if len(tel) == 0 {
 		return tel
 	}
-	code = strings.Replace(code, "+", "", -1)
+	country = strings.Replace(country, "+", "", -1)
 	tel = strings.Replace(tel, "+", "", -1)
-	if len(code) != 0 && strings.HasPrefix(tel, code) {
-		tel = tel[len(code):]
+	if len(country) != 0 && strings.HasPrefix(tel, country) {
+		tel = tel[len(country):]
 	}
 	tel = strings.Replace(strings.TrimSpace(tel), " ", "", -1)
 	return strings.Replace(tel, "-", "", -1)
