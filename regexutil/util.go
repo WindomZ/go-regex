c1a860d8e0ac544c51b26611a82c939e66c1a5e7
WindomZ <zwdsdtc@gmail.com>
2016-09-23 17:20:25 +0800

    fix: regexp util standard telephone number

--- regexutil/util.go
+++ regexutil/util.go
@@ -7,19 +7,13 @@ func StandardTelephoneNumber(country, tel string) string {
 	if len(tel) == 0 {
 		return tel
 	}
-	country = BaseCountryCode(country)
-	tel = strings.Replace(strings.TrimSpace(tel), " ", "", -1)
-	if strings.HasPrefix(tel, "+"+country) {
-	} else if len(country) != 0 {
-		if strings.HasPrefix(tel, country) {
-			tel = "+" + tel
-		} else {
-			tel = "+" + country + tel
-		}
-	} else if !strings.HasPrefix(tel, "+") {
-		tel = "+" + tel
+	tel = strings.Replace(strings.Replace(strings.TrimSpace(tel),
+		" ", "", -1), "+", "", -1)
+	if country = BaseCountryCode(country); len(country) != 0 &&
+		!strings.HasPrefix(tel, country) {
+		tel = country + tel
 	}
-	return strings.Replace(tel, "-", "", -1)
+	return "+" + strings.Replace(tel, "-", "", -1)
 }
 
 // BaseTelephoneNumber get base telephone number from country and tel
